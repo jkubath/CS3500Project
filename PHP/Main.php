ac812f0a0f6ac189a618135297ff4511f2a5511a
Jonah Kubath <jkubath1@gmail.com>
2017-11-04 13:25:51 -0400

    Nav bar and main page
    The php files are included

---
+++ PHP/Main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Three Guys</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../CSS/NavBar.css" type="text/css">
+  <link rel="stylesheet" href="../CSS/Body.css" type="text/css">
+  <!-- <link rel="stylesheet" href="../CSS/Main.css" type="text/css"> -->
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+
+<?php
+  include "../PHP/NavBar.php";
+  include "../PHP/About.php";
+  include "../PHP/MainMenu.php";
+  ?>
+
+
+
+</body>
+</html>
---
+++ PHP/MainMenu.php
@@ -0,0 +1,12 @@
+
+<div id="menuBody" class="container-fluid">
+
+  <div class="row">
+    <div>
+      <!-- <img id="menuBackground" src="../Pictures/red.jpg" /> -->
+      <h1>Menu</h1>
+      <button href="#">View the Menu</button>
+      <h2>This is our slogal<h2>
+    </div>
+  </div>
+</div>

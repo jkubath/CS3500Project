f5e076bf04f095ba4a9dbf25a3bf7a90bf3831d5
Greg Smith <gskill95@gmail.com>
2017-11-05 14:32:03 -0500

    Better Footer & Other Stuff
    Made the footer better and changed some random things

--- PHP/Main/About.php
+++ PHP/Main/About.php
@@ -2,10 +2,13 @@
 <div id="about" class="container-fluid">
 
   <div class="row">
-    <img src="../Pictures/burger.jpg" />
+    <img src="../../Pictures/burger.jpg" />
     <div id="titleBackground">
       <h1>Three Guys</h1>
-      <h2>This is our slogal<h2>
+      <h2>"Never fresh, always frozen"<h2>
+        <div class="hrdiv">
+          <hr>
+        </div>
     </div>
   </div>
 </div>
--- PHP/Main/Main.php
+++ PHP/Main/Main.php
@@ -7,26 +7,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="../CSS/NavBar.css" type="text/css">
-  <link rel="stylesheet" href="../CSS/Body.css" type="text/css">
+  <link rel="stylesheet" href="../../CSS/Header.css" type="text/css">
+  <link rel="stylesheet" href="../../CSS/Body.css" type="text/css">
+  <link rel="stylesheet" href="../../CSS/Footer.css" type="text/css">
   <!-- <link rel="stylesheet" href="../CSS/Main.css" type="text/css"> -->
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 
-
 <?php
-  include "../PHP/NavBar.php";
-  include "../PHP/About.php";
-  include "../PHP/MainMenu.php";
-  include "../PHP/Footer.php";
+  include "../Header.php";
+  include "About.php";
+  include "CustomerReviews.php";
+  include "../Footer.php";
   ?>
-
-
 
 </body>
 </html>

6fabef41f4633a1924fa49fcc824df0e816136a8
Greg Smith <gskill95@gmail.com>
2017-11-19 20:06:54 -0500

    Contact us
    Added the contact form on the main page. Need to tweak the font sizes still

--- PHP/Main.php
+++ PHP/Main.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
   <title>Three Guys</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,8 +19,61 @@
   include "Head.php";
   include "Title.php";
   include "CustomerReviews.php";
-  include "Footer.php";
   ?>
+<!-- /////////////////////////////////////FORM///////////////////////////////////////// -->
+  <div class="container-fluid text-center contactusform ">
+    <form class="contactus" action="http://www.randyconnolly.com/tests/process.php" method="post" id="Contact">
+      <fieldset>
+        <legend>Tell us how you really feel!</legend>
 
+  <?php
+    /*PROBABLY NEED TO CHANGE THE IF STATEMENT. ALSO NEED TO CHANGE THE ACTION METHOD OF THE FORM*/
+    if (empty($_GET)) {
+
+          echo '<div class="form-group left-addon col-md-6">
+              <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                <label class="control-label">Name</label>
+                <input type="text" class="form-control" placeholder="Your Name" name="name"/>
+            </div>
+            <div class="form-group left-addon col-md-6">
+              <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                <label class="control-label">Email</label>
+                <input type="email" class="form-control" placeholder="email@gmail.com" name="email"/>
+            </div>
+            <div class="form-group left-addon col-md-6">
+              <span class="glyphicon glyphicon-earphone form-control-feedback"></span>
+                <label class="control-label">Phone</label>
+                <input type="tel" class="form-control" placeholder="123-123-1234" name="tel" />
+            </div>
+            <div class="form-group col-md-6">
+              <label class="control-label" for="contacttype">Why are you contacting us? (select one):</label>
+              <select class="form-control" id="contacttype" name="contacttype">
+                <option value="Comment">Comment</option>
+                <option value="Concern">Concern</option>
+              </select>
+            </div>';
+    }
+    else {
+
+      echo      '<div class="form-group col-md-4 col-md-offset-4 makesmall">
+              <label class="control-label" for="contacttype">Why are you contacting us? (select one):</label>
+              <select class="form-control" id="contacttype" name="contacttype">
+                <option value="Comment">Comment</option>
+                <option value="Concern">Concern</option>
+              </select>
+            </div>';
+    }
+   ?>
+      </fieldset>
+    </form>
+  <div class="form-group-ta col-md-4 col-md-offset-4">
+    <textarea rows="6" cols="100" name="comment" form="Contact" placeholder="Enter your thoughts here..."></textarea>
+  </div>
+  <div class="form-group col-md-4 col-md-offset-4 text-center">
+    <input form="Contact" type="submit" value="Submit">
+  </div>
+</div>
+<!-- /////////////////////////////////////FORM///////////////////////////////////////// -->
+<?php include "Footer.php"; ?>
 </body>
 </html>
--- PHP/Title.php
+++ PHP/Title.php
@@ -2,7 +2,7 @@
 <div id="about" class="container-fluid">
 
   <div class="row">
-    <img src="../../Pictures/burger.jpg" />
+    <img src="../Pictures/burger.jpg" />
     <div id="titleBackground">
       <h1>THREE GUYS</h1>
       <h2 style="margin: 0.1em">"Never fresh, always frozen"<h2>
